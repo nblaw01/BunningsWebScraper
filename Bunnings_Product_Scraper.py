d46bcb1ef3b08920530bf69d7ef044f34d80bf8e
Nick Lawler <s3951510@student.rmit.edu.au>
2025-07-11 11:29:42 +1000

    Adjusted to fix headless chrome

--- Bunnings_Product_Scraper.py
+++ Bunnings_Product_Scraper.py
@@ -23,13 +23,15 @@ check_run_count()
 
 # Configure Chrome to run in headless mode 
 chrome_options = Options()
-#chrome_options.add_argument("--headless=new")  
+chrome_options.add_argument("--headless=new")  
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--window-size=1920,1080")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--disable-blink-features=AutomationControlled")
 chrome_options.add_argument("--remote-debugging-port=9222")
+chrome_options.add_argument("--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36")
+
 
 # Initialise the Chrome WebDriver
 driver = webdriver.Chrome(options=chrome_options)
